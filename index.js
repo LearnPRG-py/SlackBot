f3b9633fbf4cce3f7bcda37b5fea47f998acc3f2
Aryan <aryankrishnan4b@gmail.com>
2026-06-20 11:40:39 +0530

    Initial Commit
    Add Bot

---
+++ index.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+const { Octokit }= require("@octokit/core");
+
+const octokit = new Octokit(); 
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+const log_count = 10;
+
+const repo_map = new Map();
+
+app.command("/gitlogger-add-repo", async ({command, ack, respond}) => {
+  await ack();
+  const [owner, repo, name] = command.text.trim().split(/\s+/);
+  repo_map.set(name, `${owner}/${repo}`);
+  await respond({ text: `Repo by ${owner} called ${repo} added as ${name}` });
+});
+
+app.command("/gitlogger-rm-repo", async ({command, ack, respond}) => {
+  await ack();
+  const name = command.text.trim();
+  repo_map.delete(name);
+  await respond({ text: `Repo ${name} has been removed` });
+});
+
+app.command("/gitlogger-log", async ({command, ack, respond}) => {
+  await ack();
+  const name = command.text.trim();
+  let data = repo_map.get(name);
+  const [owner, repo] = data.trim().split('/');
+  const response = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+    owner, repo, per_page: log_count,
+  });
+  const text = response.data
+  .map(c => `\`${c.sha.slice(0,7)}\` ${c.commit.message.split("\n")[0]} - ${c.commit.author.name}`)
+  .join("\n");
+  await respond({ text: `Logging last ${log_count} commits from ${name}: \n ${text}`})
+});
+
+app.command("/gitlogger-help", async ({command, ack, respond}) => {
+    await ack();
+    // TODO: Figure out a way to remove many of these escape chars.
+    await respond({ text: `Gitlogger commands:
+    \`/gitlogger-add-repo <owner> <repo> <alias_name>\` - add a GitHub repo
+    \`/gitlogger-log <alias_name>\` - show recent commits
+    \`/gitlogger-remove-repo <alias_name>\` - remove a repo`})
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
