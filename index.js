1513bd29de7b5e7bda1030d604156dd93f267123
Aryan <aryankrishnan4b@gmail.com>
2026-06-20 11:57:38 +0530

    Add better error handling

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ const repo_map = new Map();
 app.command("/gitlogger-add-repo", async ({command, ack, respond}) => {
   await ack();
   const [owner, repo, name] = command.text.trim().split(/\s+/);
+  if (!(repo && owner && name)) {
+    await respond({ text: `Please enter a value for all inputs!` });
+  }
+  if (repo_map.has(name)) {
+    await respond({ text: `Repo ${name} already exists!` });
+    return;
+  }
   repo_map.set(name, `${owner}/${repo}`);
   await respond({ text: `Repo by ${owner} called ${repo} added as ${name}` });
 });
@@ -25,7 +32,15 @@ app.command("/gitlogger-add-repo", async ({command, ack, respond}) => {
 app.command("/gitlogger-rm-repo", async ({command, ack, respond}) => {
   await ack();
   const name = command.text.trim();
-  repo_map.delete(name);
+  if (!name) {
+    await respond({ text: `Usage: /gitlogger-rm-repo <name>` });
+    return;
+  }
+  const existed = repo_map.delete(name);
+  if (!existed) {
+    await respond({ text: `Repo ${name} not found!` });
+    return;
+  }
   await respond({ text: `Repo ${name} has been removed` });
 });
 
@@ -33,14 +48,25 @@ app.command("/gitlogger-log", async ({command, ack, respond}) => {
   await ack();
   const name = command.text.trim();
   let data = repo_map.get(name);
+  if (!data) {
+    await respond({text: `Repo with name ${name} hasn't been added yet!`});
+  }
   const [owner, repo] = data.trim().split('/');
-  const response = await octokit.request("GET /repos/{owner}/{repo}/commits", {
-    owner, repo, per_page: log_count,
-  });
-  const text = response.data
-  .map(c => `\`${c.sha.slice(0,7)}\` ${c.commit.message.split("\n")[0]} - ${c.commit.author.name}`)
-  .join("\n");
-  await respond({ text: `Logging last ${log_count} commits from ${name}: \n ${text}`})
+  try {
+        const response = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+        owner, repo, per_page: log_count,
+    });
+    const text = response.data
+    .map(c => `\`${c.sha.slice(0,7)}\` ${c.commit.message.split("\n")[0]} - ${c.commit.author.name}`)
+    .join("\n");
+  }
+  catch {
+    await respond({ text: `Fetching git log failed! Few things you can do include:
+                            1. Try again after ~5 minutes (API might be rate limited)
+                            2. Double check the owner annd repo provided.
+                            3. Remove and re-add the repo.`})
+  }
+  text ? await respond({ text: `Logging last ${log_count} commits from ${name}: \n ${text}`}) : await respond({ text: `Logging last ${log_count} commits from ${name} failed!`});
 });
 
 app.command("/gitlogger-help", async ({command, ack, respond}) => {
